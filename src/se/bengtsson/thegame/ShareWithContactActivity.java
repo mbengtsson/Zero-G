cacf99cc44ca83e8945704eeba88f9fff8642f2e
Marcus Bengtsson <shiho.nuki@gmail.com>
2014-04-02 10:23:41 +0200

    Removed commented-out code

--- src/se/bengtsson/thegame/ShareWithContactActivity.java
+++ src/se/bengtsson/thegame/ShareWithContactActivity.java
@@ -10,47 +10,6 @@ public class ShareWithContactActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_share_with_contact);
 
-		// ContentResolver contentResolver = getContentResolver();
-		// Cursor cursor =
-		// contentResolver.query(android.provider.ContactsContract.CommonDataKinds.Email.CONTENT_URI,
-		// null, null,
-		// null, null);
-		//
-		// ArrayAdapter<String> contactsAdapter = new ArrayAdapter<String>(this,
-		// android.R.layout.simple_list_item_1);
-		// ListView contactsList = (ListView) findViewById(R.id.contacts_list);
-		// contactsList.setAdapter(contactsAdapter);
-		// contactsList.setOnItemClickListener(this);
-		//
-		// Set<String> noDuplicates = new HashSet<String>();
-		// while (cursor.moveToNext()) {
-		// int emailId =
-		// cursor.getColumnIndex(android.provider.ContactsContract.CommonDataKinds.Email.DATA);
-		// int contactId =
-		// cursor.getColumnIndex(android.provider.ContactsContract.Contacts.DISPLAY_NAME);
-		// String emailAdress = cursor.getString(emailId);
-		// String contactName = cursor.getString(contactId);
-		// if (emailAdress != null && noDuplicates.add(emailAdress)) {
-		// contactsAdapter.add(contactName + "\n" + emailAdress);
-		// }
-		// }
-
 	}
-
-	// @Override
-	// public void onItemClick(AdapterView<?> parent, View view, int position,
-	// long id) {
-	//
-	// String item = ((TextView) view).getText().toString();
-	// String emailAddress = item.substring(item.indexOf('\n') + 1);
-	//
-	// Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto",
-	// emailAddress, null));
-	// intent.putExtra(Intent.EXTRA_SUBJECT, "Check this game out");
-	// intent.putExtra(Intent.EXTRA_TEXT,
-	// "Hi, download \"Zero-G\", it's awesome!!");
-	//
-	// startActivity(Intent.createChooser(intent, "Send mail..."));
-	// }
 
 }
--- src/se/bengtsson/thegame/fragments/MainFragment.java
+++ src/se/bengtsson/thegame/fragments/MainFragment.java
@@ -14,47 +14,7 @@ public class MainFragment extends Fragment {
 
 		View view = inflater.inflate(R.layout.fragment_main, container, false);
 
-		// view.findViewById(R.id.single_player_button).setOnClickListener(this);
-		// view.findViewById(R.id.multi_player_button).setOnClickListener(this);
-		// view.findViewById(R.id.share_button).setOnClickListener(this);
-
 		return view;
 	}
-
-	// public void singlePlayerClick() {
-	// Intent intent = new Intent(getActivity(),
-	// SingleplayerGameActivity.class);
-	// startActivity(intent);
-	// }
-	//
-	// public void multiPlayerClick() {
-	// Intent intent = new Intent(getActivity(), MultiPlayerActivity.class);
-	// startActivity(intent);
-	// }
-	//
-	// public void shareClick() {
-	// Intent intent = new Intent(getActivity(),
-	// ShareWithContactActivity.class);
-	// startActivity(intent);
-	// }
-	//
-	// @Override
-	// public void onClick(View v) {
-	// switch (v.getId()) {
-	// case R.id.single_player_button:
-	// singlePlayerClick();
-	// break;
-	// case R.id.multi_player_button:
-	// multiPlayerClick();
-	// break;
-	// case R.id.share_button:
-	// shareClick();
-	// break;
-	// default:
-	// Log.e("MainFragment", "No valid button clicked");
-	// break;
-	// }
-	//
-	// }
 
 }
